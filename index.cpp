88047032a1441dc97fd1fe33aa81700edd3d79ba
NaufalHD12 <hadinaufal06@gmail.com>
2023-03-25 11:43:44 +0700

    pointer untuk akses struct

--- index.cpp
+++ index.cpp
@@ -4,15 +4,14 @@ using namespace std;
 // PENGGUNAAN STRUCT
 struct produk
 {
-    string nama; 
+    string nama;
     string deskripsi;
     double harga;
     int stok;
 };
 
 produk listProduk[] = {
-    {"Hoodie Naruto", "xxxxxxxxxxxxxx", 50000, 32}, {"Kaos Mixue", "xxxxxxxxxxxxxx", 72340, 20}
-};
+    {"Hoodie Naruto", "xxxxxxxxxxxxxx", 50000, 32}, {"Kaos Mixue", "xxxxxxxxxxxxxx", 72340, 20}};
 
 struct transaksi
 {
@@ -22,17 +21,19 @@ struct transaksi
     bool status_pembayaran;
 };
 
-
-int main (){
-    int size = sizeof(listProduk)/sizeof(listProduk[0]);
+int main()
+{
+    int size = sizeof(listProduk) / sizeof(listProduk[0]);
 
     cout << "Daftar Produk" << endl;
 
     for (int i = 0; i < size; i++)
     {
-        cout << i+1 << ". " << listProduk[i].nama << endl << "   Deskripsi \t\t:" << listProduk[i].deskripsi << endl << "   Harga \t\t:" << listProduk[i].harga << endl << "   Stok \t\t:" << listProduk[i].stok << endl << endl; 
+        produk *pProduk = &listProduk[i]; // pointer untuk mengakses struct produk
+        cout << i + 1 << ". " << pProduk->nama << endl
+             << "   Deskripsi \t\t:" << pProduk->deskripsi << endl
+             << "   Harga \t\t:" << pProduk->harga << endl
+             << "   Stok \t\t:" << pProduk->stok << endl
+             << endl;
     }
-
-    
-
 }
