a053427f9667b52f0c042a2779277f45824a07e5
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2023-03-25 11:46:05 +0700

    menambahkan fitur login dan register

--- index.cpp
+++ index.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string> // untuk kebutuhan registrasi & login
+#include <fstream> // untuk kebutuhan registrasi & login
 using namespace std;
 
 // PENGGUNAAN STRUCT
@@ -23,8 +25,85 @@ struct transaksi
 };
 
 
+
+
+// PENGGUNAAN VOID UNTUK REGISTER
+void registrasi() {
+    string username, pass;
+    cout << "Username\t:"; cin >> username;
+    cout << "Password\t:"; cin >> pass;
+
+    ofstream userFile("dataUser.txt", ios::app);
+    if (userFile.is_open())
+    {
+        userFile << username << " " << pass << endl;
+        userFile.close();
+        cout << "Registrasi Berhasil" << endl;
+    }
+    else 
+    {
+        cout << "Something Went Wrong!" << endl;
+    }
+}
+
+// PENGGUNAAN VOID UNTUK LOGIN 
+void login() {
+    string username, pass, line;
+    bool check = false;
+    cout << "Username\t:"; cin >> username;
+    cout << "Password\t;"; cin >> pass;
+
+    ifstream userFile("dataUser.txt");
+    if (userFile.is_open())
+    {
+        while (getline(userFile, line))
+        {
+            size_t pos = line.find(" ");
+            string u = line.substr(0, pos);
+            string p = line.substr(pos+1);
+            if (u == username && p == pass)
+            {
+                check = true;
+                break;
+            }
+            
+        }
+        userFile.close();
+        if (check)
+        {
+            cout << "Login Berhasil" << endl;
+        }
+        else 
+        {
+            cout << "Username / Password salah" << endl;
+        }
+        
+    }
+    else
+    {
+        cout << "Something Went Wrong!" << endl;
+    }
+    
+}
+
 int main (){
     int size = sizeof(listProduk)/sizeof(listProduk[0]);
+    int pilih;
+
+    cout << "Silahkan Login / Register Terlebih dulu\n" << "1. Login\n" << "2. Register\n" ; cin >> pilih; 
+    if (pilih == 1)
+    {
+        login();
+    }
+    else if (pilih == 2)
+    {
+        registrasi();
+    }
+    else {
+        cout << "Masukan inputan yang benar" << endl;
+    }
+
+
 
     cout << "Daftar Produk" << endl;
 
@@ -33,6 +112,5 @@ int main (){
         cout << i+1 << ". " << listProduk[i].nama << endl << "   Deskripsi \t\t:" << listProduk[i].deskripsi << endl << "   Harga \t\t:" << listProduk[i].harga << endl << "   Stok \t\t:" << listProduk[i].stok << endl << endl; 
     }
 
-    
 
 }
