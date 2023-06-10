d8afe57bf70ece00acbd3f4bf0b4bd8ee3512bad
Muhammad Tsaqiif Ash-Shiddiq <128771221+Kanaieu@users.noreply.github.com>
2023-06-10 18:25:57 +0700

    Penambahan Notifikasi

--- index.cpp
+++ index.cpp
@@ -263,8 +263,6 @@ void minusStok(tree *root, int pil, int minStok, bool done)
     {
         if (root->data.id == pil)
         {
-            cout << minStok << endl;
-            cout << root->data.stok << endl;
             root->data.stok = root->data.stok - minStok;
             done = true;
         }
@@ -364,6 +362,7 @@ void checkPembayaran()
             cout << "\033[0m";
         }
     }
+    cout << endl;
 }
 
 // MEMBUAT LIST KOSONG
@@ -849,6 +848,7 @@ int menuUtama(){
         cout << "                                     |            ONLINE SALES PROGRAM             |" << endl;
         cout << "                                     ===============================================" << endl << endl << endl;
         cout << "\033[0m";
+        cout << "Notifikasi:" << endl;
         checkPembayaran();
         cout << "========= MENU UTAMA =========" << endl;
         cout << "[1] Melihat Daftar Produk     " << endl
